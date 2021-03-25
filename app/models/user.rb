3fe4a889dd93007e56041994996c9466cb4af4df
Krae <kraewind@gmail.com>
2021-03-25 15:57:53 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def take_ride
+        if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+            "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+        elsif self.user.tickets < self.attraction.tickets
+            "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+        elsif self.user.height < self.attraction.min_height
+            "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+        else
+            
+        end
+    end
 end
